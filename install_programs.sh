64a94166fe4339bbeb7d85b348208745a1b335d2
RobinBauwens <robin.bauwens.w0458@student.hogent.be>
2017-08-07 18:29:40 +0200

    Update install_programs.sh

--- install_programs.sh
+++ install_programs.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
-sudo apt install nodejs
+#curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+#sudo apt install nodejs
+
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python get-pip.py
+
+sudo apt-get install freetype-devel
+sudo apt-get install pkg-config
+sudo apt-get install libpng-dev libjpeg8-dev libfreetype6-dev
+pip install matplotlib
+
+pip --no-cache-dir install matplotlib #voor raspberry pi, te weinig geheugen om tijdelijk op te slaan
+sudo apt-get install python-matplotlib
