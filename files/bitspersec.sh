1644f19efe8ca205ea6091702ca8af61d46c41de
RobinBauwens <robin.bauwens.w0458@student.hogent.be>
2017-08-07 22:31:02 +0200

    Na verbetering ShellCheck

--- files/bitspersec.sh
+++ files/bitspersec.sh
@@ -8,25 +8,23 @@ set -o pipefail # don't hide some errors in pipes
 
 if [ "$#" -eq 0 ]; then # Indien geen argumenten meegegeven
 	interface="wlan0"
-	echo "Netwerkinterface niet ingegeven, standaard=${interface}"
+	echo "Netwerkinterface niet ingegeven, standaardinterface=${interface}"
 else 
 	interface="$1"
 	if [ ! -d "/sys/class/net/$1" ]; then # Indien directory niet bestaat
 		interface="wlan0"
-		echo "Directory /sys/class/net/$1 bestaat niet, standaard=${interface}"
-	else 
-		interface="$1"	
+		echo "Directory /sys/class/net/$1 bestaat niet, omgezet naar interface ${interface}"
 	fi
 fi
 
 
 while true
 	do
-	ontvangen1=$(cat /sys/class/net/$interface/statistics/rx_packets) # ('test' || "test") != $(test)
-	verzonden1=$(cat /sys/class/net/$interface/statistics/tx_packets)
+	ontvangen1=$(cat /sys/class/net/"$interface"/statistics/rx_packets) # ('test' || "test") != $(test)
+	verzonden1=$(cat /sys/class/net/"$interface"/statistics/tx_packets)
 	sleep 1 # 1 seconde wachten, meten bits/s
-	ontvangen2=$(cat /sys/class/net/$interface/statistics/rx_packets)
-	verzonden2=$(cat /sys/class/net/$interface/statistics/tx_packets)
+	ontvangen2=$(cat /sys/class/net/"$interface"/statistics/rx_packets)
+	verzonden2=$(cat /sys/class/net/"$interface"/statistics/tx_packets)
 
 	let verzonden=$verzonden2-$verzonden1
 	let ontvangen=$ontvangen2-$ontvangen1
