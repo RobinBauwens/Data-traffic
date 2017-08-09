754085a47dc9e94af5caf1e5cd8dc03820dc4691
RobinBauwens <robin.bauwens.w0458@student.hogent.be>
2017-08-09 16:37:44 +0200

    Formattering door Beautify-plugin Brackets

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Er worden een aantal statistieken (bestanden) beschikbaar gesteld in `sys/class/
 
 Bij deze applicatie zal er enkel gekeken worden naar netwerkinterface `wlan0`. Adhv een select-option in HTML (client-side) zou men eventueel de gewenste interface kunnen kiezen (zie generatie in PHP-code) maar deze informatie kan niet opnieuw behandeld worden aan de server-side.
 
+Werkt momenteel enkel voor UNIX-like systemen met package manager zoals Ubuntu/Debian (`apt-get`).
+
 [Link voor documentatie class net statistics](https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-net-statistics)
 
 ![Demonstratie applicatie](img/Demo.PNG)
--- files/data-traffic/js/data.js
+++ files/data-traffic/js/data.js
@@ -1,75 +1,75 @@
-window.onload= function(){
-   var chart = new Highcharts.Chart({
-        chart: {
-            renderTo: 'container',
-            type: 'area',
-            events: {
-                load: requestBytes 	// 1 functie die andere functies aanroept
-            }
+window.onload = function () {
+ var chart = new Highcharts.Chart({
+  chart: {
+   renderTo: 'container',
+   type: 'area',
+   events: {
+    load: requestBytes // 1 functie die andere functies aanroept
+   }
+  },
+  title: {
+   text: 'Live netwerkverkeer'
+  },
+  xAxis: {
+   type: 'datetime',
+   tickPixelInterval: 150,
+   maxZoom: 20 * 1000
+  },
+  yAxis: {
+   minPadding: 0.2,
+   maxPadding: 0.2,
+   title: {
+    text: 'Grootte bytes',
+    margin: 80
+   }
+  },
+  series: [{
+    name: 'Ontvangen bytes',
+    data: []
         },
-        title: {
-            text: 'Live netwerkverkeer'
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150,
-            maxZoom: 20 * 1000
-        },
-        yAxis: {
-            minPadding: 0.2,
-            maxPadding: 0.2,
-            title: {
-                text: 'Grootte bytes',
-                margin: 80
-            }
-        },
-        series: [{
-            name: 'Ontvangen bytes',
-            data: []
-        },
-        {
-            name: 'Verstuurde bytes',
-            data: []
+   {
+    name: 'Verstuurde bytes',
+    data: []
         }
 
         ]
-    });
+ });
 
-   var chart2 = new Highcharts.Chart({
-        chart: {
-            renderTo: 'container2',
-            type: 'spline',
-            events: {
-                load: requestPackets 	// 1 functie die andere functies aanroept
-            }
+ var chart2 = new Highcharts.Chart({
+  chart: {
+   renderTo: 'container2',
+   type: 'spline',
+   events: {
+    load: requestPackets // 1 functie die andere functies aanroept
+   }
+  },
+  title: {
+   text: 'Live netwerkverkeer'
+  },
+  xAxis: {
+   type: 'datetime',
+   tickPixelInterval: 150,
+   maxZoom: 20 * 1000
+  },
+  yAxis: {
+   minPadding: 0.2,
+   maxPadding: 0.2,
+   title: {
+    text: 'Aantal packets',
+    margin: 80
+   }
+  },
+  series: [{
+    name: 'Ontvangen packets',
+    data: []
         },
-        title: {
-            text: 'Live netwerkverkeer'
-        },
-        xAxis: {
-            type: 'datetime',
-            tickPixelInterval: 150,
-            maxZoom: 20 * 1000
-        },
-        yAxis: {
-            minPadding: 0.2,
-            maxPadding: 0.2,
-            title: {
-                text: 'Aantal packets',
-                margin: 80
-            }
-        },
-        series: [{
-            name: 'Ontvangen packets',
-            data: []
-        },
-        {
-            name: 'Verstuurde packets',
-            data: []
+   {
+    name: 'Verstuurde packets',
+    data: []
         }
 
         ]
-    });
+ });
 
 
 
@@ -77,92 +77,90 @@ window.onload= function(){
 
 
 
-  function requestPackets(){
-  	requestDataRX_Packets();
-  	requestDataTX_Packets();
-  }
+ function requestPackets() {
+  requestDataRX_Packets();
+  requestDataTX_Packets();
+ }
 
-  function requestBytes(){
-  	requestDataRX_Bytes();
-  	requestDataTX_Bytes();
-  }
-
-
-    function requestDataRX_Bytes() {
-    $.ajax({
-        url: 'live-rx_bytes.php',
-        success: function(point) {
-            var series = chart.series[0],
-                shift = series.data.length > 20; // shift if the series is 
-                                                 // longer than 20
-            // add the point
-            chart.series[0].addPoint(point, true, shift);
-            
-            // call it again after one second
-            setTimeout(requestDataRX_Bytes, 1000);    
-        },
-        cache: false
-    });
-}
-
-  function requestDataTX_Bytes() {
-    $.ajax({
-        url: 'live-tx_bytes.php',
-        success: function(point) {
-            var series = chart.series[1],
-                shift = series.data.length > 20; // shift if the series is 
-                                                 // longer than 20
-
-            // add the point
-            chart.series[1].addPoint(point, true, shift);
-            
-            // call it again after one second
-            setTimeout(requestDataTX_Bytes, 1000);    
-        },
-        cache: false
-    });
-}
+ function requestBytes() {
+  requestDataRX_Bytes();
+  requestDataTX_Bytes();
+ }
 
 
+ function requestDataRX_Bytes() {
+  $.ajax({
+   url: 'live-rx_bytes.php',
+   success: function (point) {
+    var series = chart.series[0],
+     shift = series.data.length > 20; // shift if the series is 
+    // longer than 20
+    // add the point
+    chart.series[0].addPoint(point, true, shift);
 
-    function requestDataRX_Packets() {
-    $.ajax({
-        url: 'live-rx_packets.php',
-        success: function(point) {
-            var series = chart.series[0],
-                shift = series.data.length > 20; // shift if the series is 
-                                                 // longer than 20
-            // add the point
-            chart2.series[0].addPoint(point, true, shift); // Opgelet: chart2 ipv chart
-            
-            // call it again after one second
-            setTimeout(requestDataRX_Packets, 1000);    
-        },
-        cache: false
-    });
-}
+    // call it again after one second
+    setTimeout(requestDataRX_Bytes, 1000);
+   },
+   cache: false
+  });
+ }
 
-  function requestDataTX_Packets() {
-    $.ajax({
-        url: 'live-tx_packets.php',
-        success: function(point) {
-            var series = chart.series[1],
-                shift = series.data.length > 20; // shift if the series is 
-                                                 // longer than 20
+ function requestDataTX_Bytes() {
+  $.ajax({
+   url: 'live-tx_bytes.php',
+   success: function (point) {
+    var series = chart.series[1],
+     shift = series.data.length > 20; // shift if the series is 
+    // longer than 20
 
-            // add the point
-            chart2.series[1].addPoint(point, true, shift);
-            
-            // call it again after one second
-            setTimeout(requestDataTX_Packets, 1000);    
-        },
-        cache: false
-    });
-}
- 
+    // add the point
+    chart.series[1].addPoint(point, true, shift);
+
+    // call it again after one second
+    setTimeout(requestDataTX_Bytes, 1000);
+   },
+   cache: false
+  });
+ }
 
 
-//https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-net-statistics
+
+ function requestDataRX_Packets() {
+  $.ajax({
+   url: 'live-rx_packets.php',
+   success: function (point) {
+    var series = chart.series[0],
+     shift = series.data.length > 20; // shift if the series is 
+    // longer than 20
+    // add the point
+    chart2.series[0].addPoint(point, true, shift); // Opgelet: chart2 ipv chart
+
+    // call it again after one second
+    setTimeout(requestDataRX_Packets, 1000);
+   },
+   cache: false
+  });
+ }
+
+ function requestDataTX_Packets() {
+  $.ajax({
+   url: 'live-tx_packets.php',
+   success: function (point) {
+    var series = chart.series[1],
+     shift = series.data.length > 20; // shift if the series is 
+    // longer than 20
+
+    // add the point
+    chart2.series[1].addPoint(point, true, shift);
+
+    // call it again after one second
+    setTimeout(requestDataTX_Packets, 1000);
+   },
+   cache: false
+  });
+ }
+
+
+
+ //https://www.kernel.org/doc/Documentation/ABI/testing/sysfs-class-net-statistics
 };
-
-
