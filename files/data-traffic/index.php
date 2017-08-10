b1bfe935412319e620c47567e46ea65d510a698d
RobinBauwens <robin.bauwens.w0458@student.hogent.be>
2017-08-10 16:43:17 +0200

    Code uitgebreid voor Windows

--- files/data-traffic/index.php
+++ files/data-traffic/index.php
@@ -35,16 +35,30 @@ echo "Error : $error<br>\n";
 exit;
 }
 */
+  echo "<br>";
 
+$psPath = 'c:\\Windows\\System32\WindowsPowerShell\v1.0\\powershell.exe';
+$psDIR = "D:\\XAMPP\\XAMPP\\htdocs\\data-traffic\\";
+$psScript = "show_active_interface.ps1";
+$runCMD = $psPath. ' -ExecutionPolicy RemoteSigned '.$psDIR.$psScript;
+
+
+exec($runCMD, $out);
+$intnaam=join($out);
+
+	echo "<h2 class=text-center>Gegevens van netwerk: ", $intnaam,"</h2>";
 
 if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-    echo 'This is a server using Windows!';
-    echo exec("ping 0.0.0.0");
+    //echo 'This is a server using Windows!';
+
+    #$bytes=shell_exec('powershell -File C:\D:\XAMPP\XAMPP\htdocs\data-traffic\show_active_interface.ps1');
+
+    #echo $bytes;
 
 } else {
 
     echo 'This is a server not using Windows!';
-    echo "<br>";
+ 
 	$int=exec("ip -d link | grep 'state UP' | cut -d' ' -f2 | sed 's/://g'");
 	echo "<h2 class=text-center>Gegevens van interface: ", $int,"</h2>";
 
@@ -52,4 +66,16 @@ if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 }
 
 
+
+
+/*
+Om te testen
+Gebruik echo shell_exec ipv exec met output
+$psPath = 'c:\\Windows\\System32\WindowsPowerShell\v1.0\\powershell.exe';
+$psDIR = "D:\\XAMPP\\XAMPP\\htdocs\\data-traffic\\";
+$psScript = "show_rx_bytes.ps1";
+$runCMD = $psPath. ' -ExecutionPolicy RemoteSigned '.$psDIR.$psScript;
+
+echo shell_exec($runCMD);
+*/
 ?>
